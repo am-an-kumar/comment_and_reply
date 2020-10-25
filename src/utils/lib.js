6944be72158534860391221ae76b60186ca18fd6
Aman <amankumar.edith@gmail.com>
2020-10-26 03:19:10 +0530

    Add method to format date

--- src/utils/lib.js
+++ src/utils/lib.js
@@ -16,7 +16,46 @@ function saveCommentsToLocalStorage(comments) {
     localStorage.setItem(localStorageKey, commentsJSON)
 }
 
-function getFormattedDate(dateMs) {}
+function getMonthName(monthIndex) {
+    switch (monthIndex) {
+        case 0:
+            return 'Jan'
+        case 1:
+            return 'Feb'
+        case 2:
+            return 'Mar'
+        case 3:
+            return 'Apr'
+        case 4:
+            return 'May'
+        case 5:
+            return 'Jun'
+        case 6:
+            return 'Jul'
+        case 7:
+            return 'Aug'
+        case 8:
+            return 'Sep'
+        case 9:
+            return 'Oct'
+        case 10:
+            return 'Nov'
+        case 11:
+            return 'Dec'
+        default:
+            return ''
+    }
+}
+
+function getFormattedDate(dateMs) {
+    const date = new Date(dateMs)
+
+    const day = date.getDate()
+    const monthIndex = date.getMonth()
+    const year = date.getFullYear()
+
+    return `${day} ${getMonthName(monthIndex)} ${year}`
+}
 
 export {
     getCommentsFromLocalStorage,
